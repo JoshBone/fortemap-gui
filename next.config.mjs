4b200ff2c1968a486ccadafc668ad1d15396ee1d
Josh <jozsef.bone@gmail.com>
2024-08-05 22:53:25 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -49,8 +49,7 @@ const nextConfig = {
   assetPrefix: isProd ? 'https://fmt.nektonik.com/fortemap' : undefined,
   experimental: {
     esmExternals: "loose",
-  },
-  basePath: isProd ? '/fortemap' : ''
+  }
 };
 
 export default nextConfig;
