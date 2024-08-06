42ee8c09651459d6bac82a634ee008300780cdc3
Josh <jozsef.bone@gmail.com>
2024-08-06 23:53:27 +0200

    Update queryModifiers.js

--- src/utils/queryModifiers.js
+++ src/utils/queryModifiers.js
@@ -1,25 +1,21 @@
 export const addQueryParam = (param, value, router) => {
-    const isProd = process.env.NODE_ENV === 'production'
-
     const { pathname, query } = router;
     query[param] = value
     const params = new URLSearchParams(query);
     router.replace(
-        { pathname: isProd ? `/fortemap${pathname}` : pathname, query: params.toString() },
+        { pathname, query: params.toString() },
         undefined,
-        { shallow: true }
+        { shallow: false }
     );
 };
 
 export const removeQueryParam = (param, router) => {
-    const isProd = process.env.NODE_ENV === 'production'
-
     const { pathname, query } = router;
     const params = new URLSearchParams(query);
     params.delete(param);
     router.replace(
-        { pathname: isProd ? `/fortemap${pathname}` : pathname, query: params.toString() },
+        { pathname, query: params.toString() },
         undefined,
-        { shallow: true }
+        { shallow: false }
     );
 };
