d837900435f60f6647ca2d80e70ba2273a94e4a4
Josh <jozsef.bone@gmail.com>
2024-08-14 23:51:11 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Start() {
           <div style={{padding: '20px'}}>
               <h2>Üdvözöl a Fortemap Geotagger!</h2>
               <p>Ide jön majd statisztika, meg az előre definiált menük, de most
-                  egyelőre <a href={'/photos'}>csak vigyél a fotókhoz</a></p>
+                  egyelőre <a href={'/fortemap/photos'}>csak vigyél a fotókhoz</a></p>
           </div>
         </>
       );
