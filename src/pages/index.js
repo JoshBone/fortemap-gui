46ee2e506e5a3a308190b765b43ba52bb8bca3f2
Josh <jozsef.bone@gmail.com>
2024-09-04 13:22:52 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,22 @@ export default function Start() {
 
     return (
         <>
-          <div style={{padding: '20px'}}>
-              <h2>Üdvözöl a Fortemap Geotagger!</h2>
-              <p>Ide jön majd statisztika, meg az előre definiált menük, de most
-                  egyelőre <a href={'/fortemap/photos'}>csak vigyél a fotókhoz</a></p>
-          </div>
+            <div style={{padding: '20px'}}>
+                <h2>Üdvözöl a Fortemap Geotagger!</h2>
+                <p>Ide jön majd statisztika, miből mennyi van, de egyelőre ugorjunk oda, hogy:</p>
+                <ul>
+                    <li><a href={'/fortemap/photos'}>Az összes fénykép</a></li>
+                </ul>
+                <h3>Fényképek települések szerint</h3>
+                <ul>
+                    <li><a href={'/fortemap/photos?filter_place=Győr'}>Fényképek Győrből</a></li>
+                    <li><a href={'/fortemap/photos?filter_place=Budapest'}>Fényképek Budapestről</a></li>
+                </ul>
+                <h3>A beazonosított térképpontok szerint</h3>
+                <ul>
+                    <li><a href={'/fortemap/photos?filter_locations_count=0'}>Geolokáció nélküli fényképek</a></li>
+                </ul>
+            </div>
         </>
-      );
+    );
 }
