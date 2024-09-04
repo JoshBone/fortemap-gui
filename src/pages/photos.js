9058b8e5e389f5fbda7b0c685cbbb5a5d4ffba6f
Josh <jozsef.bone@gmail.com>
2024-09-04 13:22:32 +0200

    Normális handling tábla betöltésnél

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -38,7 +38,7 @@ export default function Photos() {
         });
         setFilters(parsedFilters);
 
-        fetchData({
+        router.isReady && fetchData({
             search: query.search,
             pagination: {
                 current: parseInt(query.page || 1),
