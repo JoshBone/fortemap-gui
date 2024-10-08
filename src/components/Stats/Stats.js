f67214a14a9e90760a35f4f9938683d358410f11
Josh <jozsef.bone@gmail.com>
2024-10-08 21:53:45 +0200

    Statisztika a főoldalra

---
+++ src/components/Stats/Stats.js
@@ -0,0 +1,61 @@
+import style from "./Stats.module.scss"
+import {useEffect, useState} from "react";
+import {Table, Tag} from "antd";
+
+const FORTEPAN_API = process.env.NEXT_PUBLIC_FORTEPAN_API;
+
+const Stats = () => {
+    const [data, setData] = useState([])
+
+    const columns = [
+        {
+            title: 'Szerkesztő',
+            dataIndex: 'editor',
+            width: 400,
+        },
+        {
+            title: () => <Tag color={'#333'}>Összes</Tag>,
+            dataIndex: 'total',
+            render: (value) => (<div style={{textAlign: 'center'}}>{value}</div>)
+        },
+        {
+            title: () => <Tag color={'#1ABC9C'}>Elhelyezve</Tag>,
+            dataIndex: 'OK',
+            render: (value) => (<div style={{textAlign: 'center'}}>{value}</div>)
+        },
+        {
+            title: () => <Tag color={'#F4D03F'}>Elhelyezésre vár</Tag>,
+            dataIndex: 'ELH_VAR',
+            render: (value) => (<div style={{textAlign: 'center'}}>{value}</div>)
+        },
+        {
+            title:  () => <Tag color={'#F73E12'}>Nincs koordináta</Tag>,
+            dataIndex: 'NK',
+            render: (value) => (<div style={{textAlign: 'center'}}>{value}</div>)
+        }
+    ]
+
+    useEffect(() => {
+        fetch(`${FORTEPAN_API}/photos/stats`)
+            .then(r => r.json())
+            .then(d => {
+                setData(d)
+            });
+    }, [])
+
+    return (
+        <div style={{paddingTop: '20px', paddingRight: '20px'}}>
+            <h2>Így állunk</h2>
+            <Table
+                rowKey={'editor'}
+                columns={columns}
+                dataSource={data}
+                pagination={false}
+                size={'small'}
+                loading={data.length === 0}
+            />
+        </div>
+    )
+}
+
+export default Stats;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,38 @@
+import {Col, Row} from "antd";
+import Stats from "@/components/Stats/Stats";
 
 
 export default function Start() {
-
     return (
-        <>
-            <div style={{padding: '20px'}}>
-                <h2>Üdvözöl a Fortemap Geotagger!</h2>
-                <p>Ide jön majd statisztika, miből mennyi van, de egyelőre ugorjunk oda, hogy:</p>
-                <ul>
-                    <li><a href={'/fortemap/photos'}>Az összes fénykép</a></li>
-                </ul>
-                <h3>Fényképek települések szerint</h3>
-                <ul>
-                    <li><a href={'/fortemap/photos?filter_place=Győr'}>Fényképek Győrből</a></li>
-                    <li><a href={'/fortemap/photos?filter_place=Budapest V.'}>Fényképek Budapestről</a></li>
-                </ul>
-                <h3>A beazonosított térképpontok szerint</h3>
-                <ul>
-                    <li><a href={'/fortemap/photos?filter_locations_count=0'}>Geolokáció nélküli fényképek</a></li>
-                </ul>
-                <h3>Felhasználói kézikönyv</h3>
-                <ul>
-                    <li>
-                        <a href={'https://docs.google.com/document/d/1UOBgILDzzj75jG8QGO3YqyflI8CZ0BJFlrCorYH8oOQ/edit?usp=sharing'}>Kézikönyv megnyitása</a>
-                    </li>
-                </ul>
-            </div>
-        </>
-    );
+        <Row style={{fontFamily: 'unset'}}>
+            <Col style={{fontFamily: 'unset'}} span={12}>
+                <div style={{padding: '20px'}}>
+                    <h2>Üdvözöl a Fortemap Geotagger!</h2>
+                    <p>Ide jön majd statisztika, miből mennyi van, de egyelőre ugorjunk oda, hogy:</p>
+                    <ul>
+                        <li><a href={'/fortemap/photos'}>Az összes fénykép</a></li>
+                    </ul>
+                    <h3>Fényképek települések szerint</h3>
+                    <ul>
+                        <li><a href={'/fortemap/photos?filter_place=Győr'}>Fényképek Győrből</a></li>
+                        <li><a href={'/fortemap/photos?filter_place=Budapest V.'}>Fényképek Budapestről</a></li>
+                    </ul>
+                    <h3>A beazonosított térképpontok szerint</h3>
+                    <ul>
+                        <li><a href={'/fortemap/photos?filter_locations_count=0'}>Geolokáció nélküli fényképek</a></li>
+                    </ul>
+                    <h3>Felhasználói kézikönyv</h3>
+                    <ul>
+                        <li>
+                            <a href={'https://docs.google.com/document/d/1UOBgILDzzj75jG8QGO3YqyflI8CZ0BJFlrCorYH8oOQ/edit?usp=sharing'}>Kézikönyv
+                                megnyitása</a>
+                        </li>
+                    </ul>
+                </div>
+            </Col>
+            <Col span={12}>
+                <Stats />
+            </Col>
+        </Row>
+)
 }
